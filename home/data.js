9961e77594e11c12dbb3d23e9b3bad02d0abfff6
lisa981106-dotcom <lisa981106@gmail.com>
2026-04-30 11:53:19 +0800

    16

---
+++ home/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "首頁",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.19313973885739344,
+        "pitch": 0.03814960946889734,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.05461290010414821,
+          "pitch": -0.38183047240292467,
+          "title": "導覽大樓入口",
+          "text": "日治時期制菸廠舊址,現為松山文創園區"
+        }
+      ]
+    }
+  ],
+  "name": "松山文創園區",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
