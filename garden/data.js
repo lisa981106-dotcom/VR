57c534a5281650d5d3204a5eb8d8ca94e6c68945
lisa981106-dotcom <lisa981106@gmail.com>
2026-05-21 11:39:42 +0800

    garden

---
+++ garden/data.js
@@ -0,0 +1,258 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "巴洛克公園",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.5352251370125813,
+        "pitch": 0.18059055465775842,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6105553052600001,
+          "pitch": 0.07926819320189793,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.8202067083700655,
+          "pitch": 0.15562747829500978,
+          "rotation": 0,
+          "target": "2-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7013718186958933,
+          "pitch": -0.1609181645568789,
+          "title": "噴水池",
+          "text": "巴洛克花園噴池造景"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "花園",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.181461651080511,
+        "pitch": -0.06968137205522673,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4610216301156997,
+          "pitch": 0.09912928813140454,
+          "rotation": 0,
+          "target": "2-2"
+        },
+        {
+          "yaw": -2.29163299489748,
+          "pitch": 0.013345002605595013,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.011660325300193,
+          "pitch": -0.2518642405541165,
+          "title": "松山菸廠展區",
+          "text": "各種流行商品快閃與展覽地"
+        }
+      ]
+    },
+    {
+      "id": "2-2",
+      "name": "花園2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.4722308789339777,
+        "pitch": 0.19840110333365502,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09953601287972802,
+          "pitch": 0.2737848709480346,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 1.7296628613189666,
+          "pitch": 0.1499704837469018,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7704268626752615,
+          "pitch": -0.15188097375441245,
+          "title": "松山菸廠展區走道",
+          "text": "日治復古裝潢,使用自然照明,環保時尚"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "南區製菸廠旁",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 3.071933797895108,
+        "pitch": 0.23224710338794807,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4061402892710575,
+          "pitch": 0.12523609618482467,
+          "rotation": 0,
+          "target": "2-2"
+        },
+        {
+          "yaw": -2.610126933116989,
+          "pitch": 0.1463164265249155,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.05293055732908947,
+          "pitch": -0.19847359325565606,
+          "title": "菸廠展區入口",
+          "text": "結合新舊設計,舉辦各類活動"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "菸廠外圍",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.7479982508547138,
+        "pitch": 0.025442610078552974,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04916304622073042,
+          "pitch": -0.018680549067992658,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.970354077960648,
+          "pitch": 0.16118464307756142,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8081958712832584,
+          "pitch": -0.12917678464554427,
+          "title": "倉庫",
+          "text": "日治時期存放治菸工具"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
