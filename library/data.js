976bbbb00d9bf81d9bb2a9bea4a56538b6f136b9
lisa981106-dotcom <lisa981106@gmail.com>
2026-05-21 12:07:42 +0800

    library

---
+++ library/data.js
@@ -0,0 +1,250 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "不只是圖書館圖書館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": -0.3829017979456495,
+        "pitch": 0.11703600636137068,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4529579144660465,
+          "pitch": 0.39200785100640445,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 1.4382386423327684,
+          "pitch": 0.00574310273085743,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.336532238190422,
+          "pitch": -0.023567630120545857,
+          "title": "室內藏書區",
+          "text": "各式書類,免費閱讀"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "走廊",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.7355250741182022,
+        "pitch": -0.26715674775186393,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.254800478502288,
+          "pitch": 0.27297464511635283,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.924945778944238,
+          "pitch": 0.027277571065130957,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2940616546361827,
+          "pitch": -0.21887260190247204,
+          "title": "展區",
+          "text": "不定期與商家聯名,舉辦各式展覽與販賣周邊商品"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "展區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -1.1789877192043257,
+        "pitch": -0.05851800318068889,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7118935633072265,
+          "pitch": 0.038509482267190975,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.7859118038060053,
+          "pitch": 0.027302317267427156,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3837938277702637,
+          "pitch": -0.3135705070776833,
+          "title": "帆布看板",
+          "text": "結合主體,為官者介紹展品相關背景"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "圖書區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.086396525232832,
+        "pitch": -0.015278504352806976,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.698188241684532,
+          "pitch": 0.06474969158370314,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -2.08163024970076,
+          "pitch": 0.04577460387661958,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7383043489344878,
+          "pitch": -0.019813056803005935,
+          "title": "書桌",
+          "text": "以日治時期澡堂遺留下來的建築物,改造而成"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "圖書館外側",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1408,
+      "initialViewParameters": {
+        "yaw": -2.703484645277239,
+        "pitch": 0.018024019463165786,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.527286959439932,
+          "pitch": 0.03576211555279407,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.7567155695100407,
+          "pitch": 0.1458923534997556,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9254872130991156,
+          "pitch": -0.39987660123463975,
+          "title": "圖書區",
+          "text": "日治時期澡堂改建"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
